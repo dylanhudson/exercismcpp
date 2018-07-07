8345d24328e2d3b46f43f6904d550ae3a9ed1237
Dylan <dylan@Dylans-MacBook-Pro.local>
2018-07-06 21:10:40 -0400

    added exception for unequal string lengths, all tests pass

--- exercism/cpp/hamming/hamming.cpp
+++ exercism/cpp/hamming/hamming.cpp
@@ -5,6 +5,9 @@
 int 
 hamming::compute(std::string a, std::string b) 
 {
+	// check strings are equal
+	if (a.length() != b.length() ) { throw std::domain_error("unequal"); }
+
 	int hamming_dist = 0;
 
 	for(int i = 0; i < a.size(); i++) {
